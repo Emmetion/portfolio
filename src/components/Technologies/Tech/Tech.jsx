96c2cfbb0df8acd038dbce20ed197f52050c958a
Emmetion <emmetwormbox6@gmail.com>
2024-07-15 21:51:45 -0400

    Added base of Technologies Page

---
+++ src/components/Technologies/Tech/Tech.jsx
@@ -0,0 +1,11 @@
+import classNames from "classnames";
+import React from "react";
+
+export default function Tech({ name, bgcolor, bordercolor, svg }) {
+  return (
+    <div className={`flex flex-row items-center justify-center w-fit border-2 text-white rounded-md shadow-md  ${bordercolor} ${bgcolor}`}>
+      <img src={svg} className="w-10 h-10 bg-white rounded-md m-1 p-1 mr-2" />
+      <h1 className="text-lg font-semibold my-auto mr-3">{name}</h1>
+    </div>
+  );
+}
--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,9 +1,256 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./Technologies.css";
+
+// Icon image imports.
+// import JavaIcon from "../../assets/technologies/JavaIcon.png";
+// import SpringBoot from "../../assets/technologies/SpringBoot.png";
+// import JUnitIcon from "../../assets/technologies/JUnit5.png";
+// import MavenIcon from "../../assets/technologies/MavenIcon.svg";
+// import PythonIcon from "../../assets/technologies/PythonIcon.png";
+// import FlaskIcon from "../../assets/technologies/FlaskIcon.png";
+// import MatPlotLibIcon from "../../assets/technologies/MatPlotLibIcon.png";
+// import HTMLCSSIcon from "../../assets/technologies/HTMLandCSSIcon.png";
+// import BootstrapIcon from "../../assets/technologies/BootstrapIcon.png";
+// import JavaScriptIcon from "../../assets/technologies/JavaScriptIcon.svg";
+// import ReactIcon from "../../assets/technologies/ReactIcon.png";
+// import AngularIcon from "../../assets/technologies/AngularIcon.png";
+// import CIcon from "../../assets/technologies/CIcon.png";
+// import GolangIcon from "../../assets/technologies/Go-Logo_Blue.svg";
+// import STM32Icon from "../../assets/technologies/STM32Icon.avif";
+// import SQLIcon from "../../assets/technologies/SQLIcon.png";
+// import PostgresSQLIcon from "../../assets/technologies/PostgreSQLIcon.png";
+// import MySQLIcon from "../../assets/technologies/MySQLIcon.png";
+// import FileIcon from "../../assets/technologies/FileIcon.png";
+// import JacksonIcon from "../../assets/technologies/JacksonIcon.webp";
+// import CSVIcon from "../../assets/technologies/CSVIcon.png";
+// End
+
+// SVGs
+
+import { FaComputer } from "react-icons/fa6";
+import JavaIcon from '../../assets/tech_svgs/java.svg';
+import PythonIcon from '../../assets/tech_svgs/Python.svg';
+import JavaScriptIcon from '../../assets/tech_svgs/JavaScript.svg';
+import HTMLIcon from '../../assets/tech_svgs/HTML5.svg';
+import CSS3Icon from '../../assets/tech_svgs/CSS3.svg';
+import GolangIcon from '../../assets/tech_svgs/golang.svg';
+import CIcon from '../../assets/tech_svgs/C.svg';
+import CPlusPlusIcon from '../../assets/tech_svgs/c++.svg';
+
+import Tech from './Tech/Tech'
+// const languages = [
+//   {
+//     imageSrc: JavaIcon,
+//     title: "Java",
+//     yearsOfExperience: "3 Years",
+//     description:
+//       "The first programming languages I learned, and where I developed most of my programming mastery.",
+//     frameworks: [
+//       {
+//         imageSrc: SpringBoot,
+//         title: "Spring Boot",
+//         yearsOfExperience: "~1 Year",
+//         description:
+//           "Used in projects for managing CRUD operations as well as Spring Data JPA for database management.",
+//       },
+//       {
+//         imageSrc: JUnitIcon,
+//         title: "JUnit 5",
+//         yearsOfExperience: "~1 Year",
+//         description:
+//           "Used in various projects of mine to cover Unit-Testing. Along with this application I frequently use Jacoco to analyze my code coverage.",
+//       },
+//       {
+//         imageSrc: MavenIcon,
+//         title: "Maven",
+//         yearsOfExperience: "~2 Year",
+//         description:
+//           "Majority of my Spigot Projets (Minecraft Plugins) utilized the Maven Platform to host and install a variaty of dependencies. A lot of my experience in Java was through Maven, Gradle I have very limited experience with.",
+//       },
+//     ],
+//   },
+//   {
+//     imageSrc: PythonIcon,
+//     title: "Python",
+//     yearsOfExperience: "3 Years",
+//     description:
+//       "I've used python in many different ways. Most often I would use to create CRUD methods to manage a database, but I've also used",
+//     frameworks: [
+//       {
+//         imageSrc: FlaskIcon,
+//         title: "Flask",
+//         yearsOfExperience: "~1/2 Year",
+//         description:
+//           "Used in various applications to develop CRUD Applications.",
+//       },
+//       {
+//         imageSrc: MatPlotLibIcon,
+//         title: "MatPlotLib",
+//         yearsOfExperience: "~1/4 Year",
+//         description:
+//           "Utilized in University classes for data analysis. Limited experience but gathered solid understanding of how powerful Python is.",
+//       },
+//     ],
+//   },
+//   {
+//     imageSrc: HTMLCSSIcon,
+//     title: "HTML & CSS",
+//     yearsOfExperience: "~2 Years",
+//     description:
+//       "Made several front-end end applications with using various libraries.",
+//     frameworks: [
+//       {
+//         imageSrc: BootstrapIcon,
+//         title: "Bootstrap",
+//         yearsOfExperience: "2 Months",
+//         description:
+//           "Utilized in the development of this website, in conjunction with react-strap.",
+//       },
+//     ],
+//   },
+//   {
+//     imageSrc: JavaScriptIcon,
+//     title: "JavaScript",
+//     yearsOfExperience: "~1 Year",
+//     description:
+//       "Used in many projects of mine, mostly for front-end development. Created a variety of applications, including this Portfolio with JSX.",
+//     frameworks: [
+//       {
+//         imageSrc: AngularIcon,
+//         title: "AngularJS",
+//         yearsOfExperience: "~1/2 Year",
+//         description:
+//           "Developed a few front-end applications with this framework, combined with SpringBoot in one of my university projects.",
+//       },
+//       {
+//         imageSrc: ReactIcon,
+//         title: "React",
+//         yearsOfExperience: "~1/2 Year",
+//         description:
+//           "Used to develop this portfolio website, and some practice apps. Also used throughout some of my university projects.",
+//       },
+//     ],
+//   },
+//   {
+//     imageSrc: CIcon,
+//     title: "C",
+//     yearsOfExperience: "~1 Year",
+//     description:
+//       "Used to develop STM board applications, and experimented with it for use in Windows applications.",
+//     frameworks: [
+//       {
+//         imageSrc: STM32Icon,
+//         title: "STM32 ",
+//         yearsOfExperience: "~1/2 Year",
+//         description:
+//           "Created a MicroController application that acted as a basic audio Mini-Player. Providing controls for both a remote and physical user. See projects below.",
+//       },
+//     ],
+//   },
+//   {
+//     imageSrc: GolangIcon,
+//     title: "Golang",
+//     yearsOfExperience: "~1 Month",
+//     description:
+//       "Language I learned over the course of a hackathon. Found the error really and return statements really unique in this language. Had a fun experience with it.",
+//     frameworks: [],
+//   },
+//   {
+//     imageSrc: SQLIcon,
+//     title: "SQL",
+//     yearsOfExperience: ">1 Year",
+//     description:
+//       "My favored database methodology. Used in various projects of mine as a database layer.",
+//     frameworks: [
+//       {
+//         imageSrc: PostgresSQLIcon,
+//         title: "PostgreSQL",
+//         yearsOfExperience: "~1/2 Year",
+//         description:
+//           "Used in a couple of projects, majority of which being university projects. Also used in hackathon project.",
+//       },
+//       {
+//         imageSrc: MySQLIcon,
+//         title: "MySQL",
+//         yearsOfExperience: "1 Year",
+//         description:
+//           "Similar experience with Postgres. Developed a lot of Spigot plugins utilizing MySQL queries for database management.",
+//       },
+//     ],
+//   },
+//   {
+//     imageSrc: FileIcon,
+//     title: "File Formats",
+//     yearsOfExperience: "Many Years",
+//     description:
+//       "Almost all my projects involved many types of file formats. Here I highlight some major data storing formats.",
+//     frameworks: [
+//       {
+//         imageSrc: JacksonIcon,
+//         title: "Jackson",
+//         yearsOfExperience: "~3 Months",
+//         description:
+//           "Various mini-projects from both University and Personal projects utilized this library as a file-storage database.",
+//       },
+//       {
+//         imageSrc: CSVIcon,
+//         title: "CSV",
+//         yearsOfExperience: "~1 Year",
+//         description: "Used in various data-analytic projects.",
+//       },
+//     ],
+//   },
+// ];
 
 export default function Technologies() {
-    return (
-        <div className='text-center'>
-            Technologies page.
+  return (
+    <div className="container mx-auto text-center md:translate-y-3 items-center">
+      <div className="">
+        <div className="h1">
+          <p className="text-6xl pb-2 pt-5 justify-center flex flex-row">
+            <FaComputer className="my-auto mt-1 mr-3" /> Technologies
+          </p>
         </div>
-    )
+      </div>
+      <div>
+        <div className=" w-100">
+          <p className="mb-0">
+            Over the course of my career, I've developed many strong skills
+          </p>
+          <p className="mb-0">
+            in various programming lanauges, softwares, and frameworks alike.
+          </p>
+          <p className="mb-0">
+            Here I'll highlight some of my greatest experiences.
+          </p>
+        </div>
+      </div>
+      <div className="text-3xl text-left w-[500px] items-center mx-auto">
+        <div className="mt-10">
+          <p>Languages</p>
+          <div className="container flex flex-wrap overflow gap-1 mt-2">
+            <Tech name="Java" bgcolor="bg-red-400" bordercolor="border-red-400" svg={JavaIcon} />
+            <Tech name="Python" bgcolor="bg-blue-400" bordercolor="border-blue-400" svg={PythonIcon} />
+            <Tech name="JavaScript" bgcolor="bg-yellow-400" bordercolor="border-yellow-400"  svg={JavaScriptIcon} />
+            <Tech name="C" bgcolor="bg-blue-400" bordercolor="border-blue-400" svg={CIcon} />
+            <Tech name="C++" bgcolor="bg-blue-400" bordercolor="border-blue-400" svg={CPlusPlusIcon} />
+            <Tech name="HTML5" bgcolor="bg-gray-400" bordercolor="border-gray-400" svg={HTMLIcon} />
+            <Tech name="CSS3" bgcolor="bg-gray-400" bordercolor="border-gray-400" svg={CSS3Icon} />
+            <Tech name="Go" bgcolor="bg-green-400" bordercolor="border-green-400" svg={GolangIcon} />
+            
+          </div>
+        </div>
+        <div className="mt-10">
+          <p>Frameworks</p>
+        </div>
+        <div className="mt-10">
+          <p>Softwares</p>
+        </div>
+        <div className="mt-10">
+          <p>Editors</p>
+        </div>
+      </div>
+    </div>
+  );
 }
