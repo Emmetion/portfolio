f9f7a40233ce6b51a7d3436649ea7ac8cd9ebd2a
Emmetion <emmetwormbox6@gmail.com>
2024-07-20 19:02:09 -0400

    prject.jsx changes

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import Tech from "../../Technologies/Tech/Tech";
 
-
 export default function Project({ name, age, type, description, techs }) {
   return (
     <div className="w-fit h-fit border-4 rounded-lg border-gray-400">
       <div className="flex flex-row gap-2 mr-1">
         <div className="flex flex-col">
-          <p className="text-3xl text-left ml-2 mt-2 min-w-56">{name}</p>
-          <p className="text-xs text-left ml-2 italic">{age} <a className="not-italic" >■</a> {type}</p>
+          <p className="text-3xl text-left ml-2 mt-2">{name}</p>
+          <p className="text-xs text-left ml-2 italic">
+            {age} <a className="">//</a> {type}
+          </p>
         </div>
         <div className="flex flex-row gap-2 h-min my-auto ml-auto flex-wrap">
           {Object.values(techs).map((val, index) => {
-            return <Tech id={index} svg={val}/>;
+            return <Tech id={index} svg={val} />;
           })}
         </div>
       </div>
-      <div className="mt-1 w-[450px]">
-        {/* Description */}
-        <p className="mx-2 text-left text-lg">{description}</p>
-      </div>
+      
+      <div className="mt-1 row-span-full">
+          {/* Description */}
+          <p className="mx-2 text-left text-lg">{description}</p>
+        </div>
     </div>
   );
 }
